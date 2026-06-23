1f2357edc1cbe49287d5f8fa834740d3dba59707
rasheed-web-dev <274648074+rasheed-web-dev@users.noreply.github.com>
2026-06-23 23:15:56 +0300

    Add hash function with modulo to avoid overflow

---
+++ hashmap.js
@@ -0,0 +1,20 @@
+class HashMap {
+  constructor() {
+    this.capacity = 16;
+    this.loadFactor = 0.75;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
+  }
+}
+
+const h = new HashMap();
+console.log(h.hash("bob"));
